ac951935fece720776b63e6e3e04924bed41cb9c
amyrbakhsh <amyr.bakhsh@gmail.com>
2025-04-27 19:20:15 +0300

    classes lab

--- classes.py
+++ classes.py
@@ -29,8 +29,7 @@ class BankAccount():
                     self.balance -= amount
                     return f"Account {self.account_no} - New balance: {self.balance} $$"
                 else:
-                    return f"Sorry you don't have enough balance {self.balance} $$ to withdraw {amount} $$"
-                
+                    return f"insufficient balance to withdraw {amount} $$ Account Balance: {self.balance} $$ "
             elif self.has_overdraft == True:
                 self.balance -= amount
                 return f"Account {self.account_no} - New balance: {self.balance} $$"
@@ -45,27 +44,26 @@ class SavingsAccount(BankAccount):
     def withdraw(self, amount):
         return "No withdrawals permitted"
    
-# account instance 1								  
-account1 = BankAccount("Charlie")
+# account1								  
+account1 = BankAccount("Charlie", 78)
 
 print(account1)
 print(account1.deposit(200))
 print(account1.withdraw(25))
 print(account1.withdraw(50))
 
-# account instance 2
-
+# account2
 account2 = BankAccount("Bob", 500)
 
 print(account2)
 print(account2.deposit(1000))
-print(account2.withdraw(50))
+print(account2.withdraw(4000))
 print(account2.deposit(200))
 
 
 # SavingsAccount
-savings1 = SavingsAccount("Jack", 90)
+savings = SavingsAccount("Jack", 20000)
 
-print(savings1)
-print(savings1.deposit(90))
-print(savings1.withdraw(12))
+print(savings)
+print(savings.deposit(1000))
+print(savings.withdraw(1500))
